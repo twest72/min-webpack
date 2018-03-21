5f47de9f74539e294de02173173772ab3399d0bf
Thomas Westphal <twestphal4.ext@gema.de>
2018-03-21 01:42:25 +0100

    adds HtmlWebpackPlugin

--- webpack.config.js
+++ webpack.config.js
@@ -1,13 +1,19 @@
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
     entry: './src/index.js',
-    devtool: 'inline-source-map',
+    // devtool: 'inline-source-map',
     devServer: {
         contentBase: './dist'
     },
+    plugins: [
+        new HtmlWebpackPlugin({
+            title: 'Webpack Starter'
+        })
+    ],
     output: {
-        filename: 'bundle.js',
+        filename: '[name].bundle.js',
         path: path.resolve(__dirname, 'dist')
     }
 };
