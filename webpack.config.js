bc0c3b79f22ccb0d5d5e1b90536edc57e846dea5
Thomas Westphal <twestphal4.ext@gema.de>
2018-03-21 01:45:34 +0100

    adds CleanWebpackPlugin

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,6 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const CleanWebpackPlugin = require('clean-webpack-plugin');
 
 module.exports = {
     entry: './src/index.js',
@@ -8,6 +9,7 @@ module.exports = {
         contentBase: './dist'
     },
     plugins: [
+        new CleanWebpackPlugin(['dist']),
         new HtmlWebpackPlugin({
             title: 'Webpack Starter'
         })
